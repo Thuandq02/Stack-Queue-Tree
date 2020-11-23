99bf4a8f6def2a71342323aa7bd4b28d69f70810
dqthuan <72246634+dqthuan@users.noreply.github.com>
2020-11-23 10:25:43 +0700

    Add files via upload

---
+++ chuyen-doi/index.php
@@ -0,0 +1,25 @@
+<?php
+include_once "ReadingList.php";
+
+$stack = new ReadingList();
+
+$number = 71;
+
+
+while (($results = $number / 2) && $results > 1) {
+    $remainder = $number % 2;
+    $number = $results;
+    $stack->push($remainder);
+    if ($results > 0 && $results < 2)
+    {
+        $stack->push(1);
+        break;
+    }
+}
+
+
+
+print("<pre>" . print_r($stack, true) . "</pre>");
+
+
+echo implode(", ", $stack->getStack());
